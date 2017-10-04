ea07dc05b197584804b8aac73609393d12bf6742
jaestrella <jaestrella1991@gmail.com>
2017-10-04 13:42:05 +0200

    Initial commit

---
+++ app/src/main/java/com/iesvirgendelcarmen/dam/fragment01/Fragmento3.java
@@ -0,0 +1,14 @@
+package com.iesvirgendelcarmen.dam.fragment01;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragmento3 extends Fragment {
+
+   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+       return inflater.inflate(R.layout.activity_fragmento3, container,false);
+   }
+}
